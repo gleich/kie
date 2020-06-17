bc83703da77751b5adb7bfa6caf22ce316dface7
Matthew Gleich <matthewgleich@gmail.com>
2020-06-17 15:57:15 -0400

    ✨ Create config.sh
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

---
+++ config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This file is to serve more as a list of all the configs you need to run. That is why it exits immediately
+
+exit
+
+# ZSH
+sudo apt-get install zsh git -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+# p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
